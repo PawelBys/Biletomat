e10af5ed059e050d7eaafe3acfb20726d82e1a0a
PawelBys <kontakt.pawel.bys@gmail.com>
2020-10-26 17:21:31 +0100

    querysset

--- generator/views.py
+++ generator/views.py
@@ -117,30 +117,34 @@ def panel(request, *args, **kwargs):
     return render(request, "panel.html", context)
 
 def rozkaz(request):
-    queryset = Dane.objects.values_list('stopien', 'imie', 'nazwisko', 'data_wyjazdu', 'data_powrotu', 'miasto') # ogranicz to do wpisów z ostatnich 3 dni
+    queryset = Dane.objects.all()
+
+
+    #queryset = Dane.objects.values_list('stopien', 'imie', 'nazwisko', 'data_wyjazdu', 'data_powrotu', 'miasto') # ogranicz to do wpisów z ostatnich 3 dni
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     generated_rozkaz = os.path.join(THIS_FOLDER, os.pardir, 'demo.docx')
     document = Document()
     table = document.add_table(rows=0, cols=7)
     lp = 1
 
-    for stopien, imie, nazwisko, data_wyjazdu, data_przyjazdu, miasto in queryset:
+    #for stopien, imie, nazwisko, data_wyjazdu, data_przyjazdu, miasto in queryset:
+    for i in queryset:
         # formatowanie daty
         data = 'w dn. '
-        if str(data_przyjazdu)[5:7] == str(data_wyjazdu)[5:7]:
-            data += str(data_wyjazdu)[8:10] + ' - '
+        if str(i.data_powrotu)[5:7] == str(i.data_wyjazdu)[5:7]:
+            data += str(i.data_wyjazdu)[8:10] + ' - '
         else:
-            data += str(data_wyjazdu)[8:10] + '.' + str(data_wyjazdu)[5:7] + ' - '
-        data += str(data_przyjazdu)[8:10] + '.' + str(data_wyjazdu)[5:7] + '.' + str(data_wyjazdu)[0:4] + ' r.'
+            data += str(i.data_wyjazdu)[8:10] + '.' + str(i.data_wyjazdu)[5:7] + ' - '
+        data += str(i.data_powrotu)[8:10] + '.' + str(i.data_wyjazdu)[5:7] + '.' + str(i.data_wyjazdu)[0:4] + ' r.'
         # wpisywanie danych do tabeli
         komorki = table.add_row().cells
         komorki[0].text = str(lp) + ')'
-        komorki[1].text = stopien
-        komorki[2].text = imie
-        komorki[3].text = str(nazwisko).upper()
+        komorki[1].text = i.stopien
+        komorki[2].text = i.imie
+        komorki[3].text = str(i.nazwisko).upper()
         komorki[4].text = data
         komorki[5].text = 'do m.'
-        komorki[6].text = miasto
+        komorki[6].text = i.miasto
         lp += 1
     #ustawianie parametrów dokumentu
     style = document.styles['Normal']
